3d0b3863a093a7fa99ef97efec6563cad009d5cc
zeyadhassan41 <zeyadhassan960@gmail.com>
2025-07-03 14:02:30 +0300

    Add programming paradigm Task 1

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,4 +13,4 @@ class BankAccount:
         return False
   
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance:.2}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
