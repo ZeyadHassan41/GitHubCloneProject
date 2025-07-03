ec62a9375bd42645ad2fbde3a0def1f39375dc8d
zeyadhassan41 <zeyadhassan960@gmail.com>
2025-07-03 23:05:55 +0300

    Add programming paradigm Task 2

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -13,7 +13,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(-5, -8), -13)
         self.assertEqual(self.calc.add(1.5, 2.3), 3.8)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(3, 5), -2)
         self.assertEqual(self.calc.subtract(0, 0), 0)
